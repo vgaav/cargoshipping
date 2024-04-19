97494007d8e638c7e75227e1034de62352d36a49
VenSalvador <raven.jssalvador@gmail.com>
2024-04-19 14:57:14 +0800

    Added styles.css

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -2,16 +2,16 @@
     <v-main>
         <v-container>
             <v-row>
-                <v-col cols="12">
-                    <v-img class="mx-auto rounded-circle" cover src="https://st3.depositphotos.com/5572200/15094/v/450/depositphotos_150942812-stock-illustration-a-man-is-riding-a.jpg"></v-img>
+                <v-col class="text-center">
+                    <v-img class="mx-auto rounded-circle" cover src="https://st3.depositphotos.com/5572200/15094/v/450/depositphotos_150942812-stock-illustration-a-man-is-riding-a.jpg" style="max-width: 200px;"></v-img>
                 </v-col>
                 <v-col cols="12">
-                    <v-card class="mx-auto px-6 py-8" elevation="16">
+                    <v-card class="mx-auto px-6 py-8 custom-rounded-card" elevation="16" >
                         <v-form>
-                            <v-text-field label="Username" placeholder="sample@gmail.com" type="email">
+                            <v-text-field label="Username" variant="outlined" placeholder="sample@gmail.com" type="email">
 
                             </v-text-field>
-                            <v-text-field label="Password" type="password">
+                            <v-text-field label="Password" variant="outlined" type="password">
 
                             </v-text-field>
                         </v-form>
@@ -20,9 +20,12 @@
                             <v-btn color="blue"> Signup</v-btn>
                             <v-btn color="blue"> Login</v-btn>
                         </v-card-actions>
+                        <v-divider></v-divider>
                     </v-card>
                 </v-col>
             </v-row>
         </v-container>
     </v-main>
 </template>
+
+<style src="/resources/css/styles.css"></style>
