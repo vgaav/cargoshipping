4879faac3973b0d9eb1a3e6658683a361fc98dc6
VGAAV <vernonvicencio03@gmail.com>
2024-04-24 11:49:47 +0800

    CarrierInfo page made web.php updated

--- resources/js/pages/CarrierInfo.vue
+++ resources/js/pages/CarrierInfo.vue
@@ -1,7 +1,35 @@
 <template>
     <v-main>
         <v-container>
-            ABCDEF
+            <v-row>
+                <v-col>
+                    <v-card title="Carrier Information" class="mx-auto px-6 py-8">
+                        <v-form>
+                            <v-text-field label="Enter Username" :width="300" />
+                            <v-text-field label="Enter Password" :width="300" />
+                            <v-text-field label="Confirm Password" :width="300" />
+                            <v-text-field label="Enter Contact No." :width="300" />
+                            <v-text-field label="Enter Email" :width="300" />
+                            <v-text-field label="Location" :width="300" />
+                            <v-select label="Province" :items="['NCR', 'Region 1', 'Region 2']" ></v-select>
+                            <v-select label="Municipality" :items="['NCR', 'Region 1', 'Region 2']" ></v-select>
+                            <v-text-field label="Postal Code" :width="300" />
+                        </v-form>
+                        <v-card-actions class="justify-end">
+                            <v-btn @click="Next">
+                                Next
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </v-main>
 </template>
+
+<script setup>
+
+function Next (){
+    this.$router.push({path: '/Login'})
+}
+</script>
