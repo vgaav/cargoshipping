dcc552d866116c60d1261ca153aab6e8a0cc4afd
maui <Omen@DESKTOP-H31QVNV>
2024-04-25 13:26:54 +0800

    Landing Page content/function complete: Styling checking needed

---
+++ resources/js/pages/carrierDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <NavBar/>
+    <v-main>
+        <v-row>
+            <v-col>
+                ABC
+            </v-col>
+        </v-row>
+    </v-main>
+</template>
+
+<script setup>
+import NavBar from '../components/NavBar.vue';
+
+</script>
