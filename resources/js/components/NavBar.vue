efc47ce9f6a6db28cbe085e104473ea7fa67d03b
VGAAV <vernonvicencio03@gmail.com>
2024-04-26 10:56:02 +0800

    router.js

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,9 +1,31 @@
 <template>
-    <v-bottom-navigation>
-        <v-row>
-            <v-col cols="12">
-                <v-btn circular icon="mdi-plus"></v-btn>
-            </v-col>
-        </v-row>
+    <v-bottom-navigation grow bg-color="primary">
+        <v-btn>
+            <v-icon>mdi-earth</v-icon>
+            <span>Map</span>
+        </v-btn>
+        <v-btn>
+            <v-icon>mdi-package</v-icon>
+            <span>Package</span>
+        </v-btn>
+        <v-btn>
+            <v-icon>mdi-plus</v-icon>
+            <span>Home</span>
+        </v-btn>
+        <v-btn>
+            <v-icon>mdi-truck</v-icon>
+            <span>Truck</span>
+        </v-btn>
+        <v-btn>
+            <v-icon>mdi-bell</v-icon>
+            <span>Notifs</span>
+        </v-btn>
     </v-bottom-navigation>
 </template>
+
+<style>
+.textwrap .v-btn__content{
+    flex-direction: column;
+}
+
+</style>
