7260b07d6c5d0427b6bc42d7a35e98e827a06c51
VGAAV <vernonvicencio03@gmail.com>
2024-04-30 10:12:22 +0800

    NavBar UI positioning fixed. Functionality next

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,24 +1,19 @@
-<template>    
+<template>
     <!-- Top Navigation -->
-    <v-btn position="fixed" size="x-large" location = "top right" class="ma-4" icon="mdi-bell"></v-btn>
+    <v-btn position="fixed" size="x-large" location = "top right" class="mx-4 my-4" icon="mdi-bell"></v-btn>
 
     <!-- Bottom Navigation-->
-    <v-btn position="fixed" size="x-large" location = "bottom end" class="ma-4" icon="mdi-menu" id="menu-activator"/>
+    <v-btn position="fixed" size="x-large" location = "bottom right" class="mx-4 my-4" icon="mdi-menu" id="menu-activator"/>
 
     <v-menu activator="#menu-activator">
-        <v-list>
-            <v-list-item>
-                <v-btn append-icon="mdi-truck">
-                    Delivery
-                </v-btn>
-                <v-btn append-icon="mdi-earth">
-                    Maps
-                </v-btn>
-            </v-list-item>
-        </v-list>
+
+        <!-- ADD COMPONENTS HERE LATERRRR-->
+        <v-btn size="x-large" icon="mdi-truck"/>
+        <v-btn size="x-large" icon="mdi-package" class="my-2"/>
     </v-menu>
 
-    <v-btn position="fixed" size="x-large" location = "bottom center" class="ma-4" icon="mdi-plus">
+    <!-- Middle Navigation-->
+    <v-btn position="fixed" size="x-large" location = "bottom center" class="my-4" icon="mdi-plus">
     </v-btn>
 
 
@@ -29,6 +24,7 @@ export default{
 	name: 'App',
 	data: () => ({
 		drawer: false,
+        navigate: false,
 	}),
 };
 </script>
