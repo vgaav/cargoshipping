dae76d45ab5abea631091c14faf3e897b77a07cd
VGAAV <vernonvicencio03@gmail.com>
2024-04-30 15:03:23 +0800

    Dashboard okay with placeholder map

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,12 +3,9 @@
         <v-container>
             <v-row>
                 <v-col>
-                    <v-img
-                        class="mx-auto rounded-circle"
-                        cover
-                        src="https://st3.depositphotos.com/5572200/15094/v/450/depositphotos_150942812-stock-illustration-a-man-is-riding-a.jpg"
-                        style="max-width: 125px"
-                    ></v-img>
+                    <v-card>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61758.453395284356!2d121.02571396334369!3d14.661426722815879!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9002bc77315%3A0x5bf0e892cd8f34bd!2smobiBREW%20Kopi!5e0!3m2!1sen!2sph!4v1714460392653!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </v-card>
                 </v-col>
             </v-row>
             <v-row>
