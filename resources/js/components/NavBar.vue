9655f7c93ac26eca6b2d8b88137d8edf6b58b4d5
VGAAV <vernonvicencio03@gmail.com>
2024-04-30 14:53:36 +0800

    NavBar UI check, UserDashboard UI check

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,30 +1,34 @@
 <template>
-    <!-- Top Navigation -->
+    <!-- Alerts -->
     <v-btn position="fixed" size="x-large" location = "top right" class="mx-4 my-4" icon="mdi-bell"></v-btn>
 
-    <!-- Bottom Navigation-->
+    <!-- Burger Menu -->
     <v-btn position="fixed" size="x-large" location = "bottom right" class="mx-4 my-4" icon="mdi-menu" id="menu-activator"/>
 
+    <!-- Burger Menu Contents -->
     <v-menu activator="#menu-activator">
-
-        <!-- ADD COMPONENTS HERE LATERRRR-->
-        <v-btn size="x-large" icon="mdi-truck"/>
+        <v-btn size="x-large" icon="mdi-truck" class="my-2"/>
+        <v-btn size="x-large" icon="mdi-earth" class="my-2"/>
         <v-btn size="x-large" icon="mdi-package" class="my-2"/>
     </v-menu>
 
-    <!-- Middle Navigation-->
-    <v-btn position="fixed" size="x-large" location = "bottom center" class="my-4" icon="mdi-plus">
+    <!-- Home Navigation -->
+    <v-btn position="fixed" size="x-large" location = "bottom center" class="mx-auto my-4" icon="mdi-plus" @click="home">
     </v-btn>
 
 
 </template>
 
-<script>
-export default{
-	name: 'App',
-	data: () => ({
-		drawer: false,
-        navigate: false,
-	}),
-};
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+//Declaration of Next v-btn to move pages
+function home() {
+  router.push({
+    name: 'LandingPage',
+  })
+}
 </script>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,17 +1,59 @@
 <template>
-    <NavBar/>
     <v-main>
         <v-container>
             <v-row>
-                <v-col class="text-center" cols="12">
-                    <v-card color="primary" class="mx-auto px-6 py-4"
-                    text="Call niyo si <NavBar/> sa component para makita"
-                    >
+                <v-col>
+                    <v-img
+                        class="mx-auto rounded-circle"
+                        cover
+                        src="https://st3.depositphotos.com/5572200/15094/v/450/depositphotos_150942812-stock-illustration-a-man-is-riding-a.jpg"
+                        style="max-width: 125px"
+                    ></v-img>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="text-center">
+                    <v-card class="mx-auto px-6 py-8">
+                        <v-btn block variant="text" prepend-icon="mdi-package" class="mx-auto px-6 py-8">
+                        Pickup Location
+                        </v-btn>
+
+                        <v-btn block variant="text" prepend-icon="mdi-truck-delivery-outline" class="mx-auto px-6 py-8">
+                        Drop-off Location
+                        </v-btn>
+
+                        <v-spacer></v-spacer>
+
+                        <v-btn block variant="flat" class="mx-auto px-6 py-8" color="primary" append-icon="mdi-plus">
+                        Add Stop
+                        </v-btn>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="text-center">
+
+                    <v-card class="mx-auto px-6 py-8">
+                        
+                        <v-card-title> Available Vehicles </v-card-title>
+
+                        <v-btn variant="text" block class="mx-auto px-6 py-8" prepend-icon="mdi-moped">
+                            Motorcycle
+                        </v-btn>
+                        
+                        <v-btn variant="text" block class="mx-auto px-6 py-8" prepend-icon="mdi-van-utility">
+                            Van
+                        </v-btn>
+
+                        <v-btn variant="text" block class="mx-auto px-6 py-8" prepend-icon="mdi-truck">
+                            Truck
+                        </v-btn>
                     </v-card>
                 </v-col>
             </v-row>
         </v-container>
     </v-main>
+    <NavBar/>
 </template>
 
 <script setup>
